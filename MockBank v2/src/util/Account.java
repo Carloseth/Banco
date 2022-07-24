b033788ba66e5bb34b0f68e7ada728b4029e0d79
kitsune <carlosufpa10@gmail.com>
2022-07-24 13:14:28 -0300

    Create account.java
    Criando metodo getBalance()

---
+++ MockBank v2/src/util/Account.java
@@ -0,0 +1,16 @@
+package util;
+
+public class Account {
+    private double balance;
+    public String name;
+
+    public double getBalance(double balance)
+    {
+        this.balance = balance;
+        return balance;
+    }
+    void transfer(double balance){
+    }
+    void withdraw(double balance){
+    }
+}
